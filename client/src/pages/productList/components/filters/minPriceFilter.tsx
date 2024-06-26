10d57882287a908c0dd1aee5404e1685ac733268
KingDiegato <gonzalezbusnego@gmail.com>
2024-06-26 11:30:45 -0300

    feat: add range input for the minprice filter

--- client/src/pages/productList/components/filters/minPriceFilter.tsx
+++ client/src/pages/productList/components/filters/minPriceFilter.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { useAppDispatch } from "../../../../hooks/redux";
 import { sortByPrice } from "../../../../redux/sort/sortSlice";
 
 export const MinPriceFilter = () => {
+  const [minPrice, setMinPrice] = useState(0);
   const dispatch = useAppDispatch();
 
   const handleMinPriceFilter = (e: string) => {
     const price = Number(e);
+    setMinPrice(price);
     dispatch(
       sortByPrice({
         minPrice: price,
@@ -13,11 +16,23 @@ export const MinPriceFilter = () => {
     );
   };
   return (
-    <>
+    <div>
       <input
         type="number"
+        name="minPrice"
+        value={minPrice}
         onChange={(e) => handleMinPriceFilter(e.target.value)}
       />
-    </>
+      <label htmlFor="rangeInput">
+        <input
+          id="rangeInput"
+          type="range"
+          min="0"
+          max="250"
+          value={minPrice}
+          onChange={(e) => handleMinPriceFilter(e.target.value)}
+        />
+      </label>
+    </div>
   );
 };
