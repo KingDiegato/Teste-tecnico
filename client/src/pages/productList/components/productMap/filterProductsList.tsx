7919415211683f3766364b12d0f240030fcb2685
KingDiegato <gonzalezbusnego@gmail.com>
2024-06-26 10:27:24 -0300

    feat: products can be sorted by most popular first

---
+++ client/src/pages/productList/components/productMap/filterProductsList.tsx
@@ -0,0 +1,62 @@
+import ProductsMock from "../../../../mock/products.json";
+import * as ProductsStyle from "./styles";
+
+import { useSelector } from "react-redux";
+import { getSortState } from "../../../../redux/sort/sortSlice";
+import { AddToFavorite } from "./addItemToWishList";
+import { Rating } from "react-simple-star-rating";
+import { AddToCart } from "./addItemToCart";
+import { useEffect, useState } from "react";
+
+export const FilterProductsList = ({ active }: { active: boolean }) => {
+  const filter = useSelector(getSortState);
+  const [initialProduct] = useState(ProductsMock.products);
+  const [sortedProducts, setSortedProducts] = useState([...initialProduct]);
+
+  useEffect(() => {
+    if (active) {
+      const sorted = initialProduct.sort((a, b) => {
+        return b.rating - a.rating;
+      });
+      setSortedProducts(sorted);
+    } else {
+      setSortedProducts(initialProduct);
+    }
+  }, [active, initialProduct]);
+
+  const Products = sortedProducts.filter((product) => {
+    if (filter.category === "all") {
+      return product.price >= filter.minPrice;
+    }
+    return (
+      product.category === filter.category && product.price >= filter.minPrice
+    );
+  });
+  return (
+    <>
+      {Products.map((product) => {
+        return (
+          <div key={product.id}>
+            <div>
+              <h1>{product.title}</h1>
+              <AddToFavorite product={product} />
+              <p>{product.description}</p>
+              <ProductsStyle.ImageContainer>
+                <img
+                  src={product.images[0] || ""}
+                  alt={product.title}
+                  width={200}
+                  height={200}
+                />
+              </ProductsStyle.ImageContainer>
+              <p>{product.price}</p>
+              <Rating readonly initialValue={product.rating} />{" "}
+              <span>{product.rating}</span>
+              <AddToCart product={product} />
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
--- client/src/pages/productList/components/productMap/products.tsx
+++ client/src/pages/productList/components/productMap/products.tsx
@@ -1,57 +1,22 @@
-import ProductsMock from "../../../../mock/products.json";
 import * as ProductsStyle from "./styles";
-import { AddToCart } from "./addItemToCart";
-import { AddToFavorite } from "./addItemToWishList";
 import { CategoryFilter } from "../filters/categoryFilter";
-import { useSelector } from "react-redux";
-import { getSortState } from "../../../../redux/sort/sortSlice";
 import { MinPriceFilter } from "../filters/minPriceFilter";
+import { useState } from "react";
+import { FilterProductsList } from "./filterProductsList";
 
 export const Products = () => {
-  const filter = useSelector(getSortState);
+  const [active, setActive] = useState<boolean>(false);
 
-  console.log(filter);
-
-  const Products = ProductsMock.products.filter((product) => {
-    if (filter.minPrice === 0) {
-      return true;
-    }
-    if (filter.category === "all") {
-      return product.price >= filter.minPrice;
-    }
-    return (
-      product.category === filter.category && product.price >= filter.minPrice
-    );
-  });
   return (
     <>
       <CategoryFilter />
       <MinPriceFilter />
+      <label>
+        <p>Mais populares primeiro</p>
+        <input type="checkbox" onChange={() => setActive(!active)} />
+      </label>
       <ProductsStyle.ProductContainer>
-        {Products &&
-          Products.map((product) => {
-            return (
-              <div key={product.id}>
-                <div>
-                  <h1>{product.title}</h1>
-                  <AddToFavorite product={product} />
-                  <p>{product.description}</p>
-                  <ProductsStyle.ImageContainer>
-                    <img
-                      src={product.images[0] || ""}
-                      alt={product.title}
-                      width={200}
-                      height={200}
-                    />
-                  </ProductsStyle.ImageContainer>
-                  <p>{product.price}</p>
-                  <p>{product.rating}</p>
-
-                  <AddToCart product={product} />
-                </div>
-              </div>
-            );
-          })}
+        <FilterProductsList active={active} />
       </ProductsStyle.ProductContainer>
     </>
   );
