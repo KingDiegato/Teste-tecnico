d9d7432d353612d768c131fa64b73d90974f3b18
KingDiegato <gonzalezbusnego@gmail.com>
2024-06-29 15:03:07 -0300

    chore: update products components to render products from the api

--- client/src/pages/productList/components/productMap/filterProductsList.tsx
+++ client/src/pages/productList/components/productMap/filterProductsList.tsx
@@ -1,4 +1,3 @@
-import ProductsMock from "../../../../mock/products.json";
 import * as ProductsStyle from "./styles";
 
 import { useSelector } from "react-redux";
@@ -6,66 +5,63 @@ import { getSortState } from "../../../../redux/sort/sortSlice";
 import { AddToFavorite } from "./addItemToWishList";
 import { Rating } from "react-simple-star-rating";
 import { AddToCart } from "./addItemToCart";
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useFetchProducts } from "../../../../hooks/useFetchProducts";
+import { Product } from "../../../../types/product";
 
-export const FilterProductsList = ({ active }: { active: boolean }) => {
-  const filter = useSelector(getSortState);
-  const [initialProduct] = useState(ProductsMock.products);
-  const [sortedProducts, setSortedProducts] = useState([...initialProduct]);
-
-  useEffect(() => {
-    if (active) {
-      const sorted = initialProduct.sort((a, b) => {
-        return b.rating - a.rating;
-      });
-      setSortedProducts(sorted);
-    } else {
-      setSortedProducts(initialProduct);
-    }
-  }, [active, initialProduct]);
-
-  const Products = sortedProducts.filter((product) => {
-    if (filter.category === "all") {
-      return product.price >= filter.minPrice;
-    }
-    return (
-      product.category === filter.category && product.price >= filter.minPrice
-    );
+export const FilterProductsList = () => {
+  const { products } = useFetchProducts({
+    url: `${import.meta.env.VITE_API_URL}products`,
   });
+
+  const filter = useSelector(getSortState);
+
+  const filteredPRoducts: Product[] = products
+    ? products.products.filter((product) => {
+        if (filter.category === "all") {
+          return product.price >= filter.minPrice;
+        }
+        return (
+          product.category === filter.category &&
+          product.price >= filter.minPrice
+        );
+      })
+    : [];
+
   return (
     <>
-      {Products.map((product) => {
-        return (
-          <ProductsStyle.CardStyle key={product.id}>
-            <ProductsStyle.ImageContainer>
-              <Link to={`/product/${product.id}`}>
-                <img
-                  src={product.thumbnail || ""}
-                  alt={product.title}
-                  width={200}
-                  height={200}
-                  loading="lazy"
-                  decoding="async"
-                />
-              </Link>
-            </ProductsStyle.ImageContainer>
-            <ProductsStyle.NameContainer>
-              <h2>
-                <Link to={`/product/${product.id}`}>{product.title}</Link>
-              </h2>
-              <AddToFavorite product={product} />
-            </ProductsStyle.NameContainer>
-            <p>{product.description}</p>
-            <p>R$ - {product.price}</p>
-            <div>
-              <Rating size={20} readonly initialValue={product.rating} />{" "}
-              <span>{product.rating}</span>
-            </div>
-            <AddToCart product={product} />
-          </ProductsStyle.CardStyle>
-        );
-      })}
+      {filteredPRoducts &&
+        filteredPRoducts.map((product) => {
+          return (
+            <ProductsStyle.CardStyle key={product.id}>
+              <ProductsStyle.ImageContainer>
+                <Link to={`/product/${product.id}`}>
+                  <img
+                    src={product.thumbnail || ""}
+                    alt={product.title}
+                    width={200}
+                    height={200}
+                    loading="lazy"
+                    decoding="async"
+                  />
+                </Link>
+              </ProductsStyle.ImageContainer>
+              <ProductsStyle.NameContainer>
+                <h2>
+                  <Link to={`/product/${product.id}`}>{product.title}</Link>
+                </h2>
+                <AddToFavorite product={product} />
+              </ProductsStyle.NameContainer>
+              <p>{product.description}</p>
+              <p>R$ - {product.price}</p>
+              <div>
+                <Rating size={20} readonly initialValue={product.rating} />{" "}
+                <span>{product.rating}</span>
+              </div>
+              <AddToCart product={product} />
+            </ProductsStyle.CardStyle>
+          );
+        })}
     </>
   );
 };
--- client/src/pages/productList/components/productMap/products.tsx
+++ client/src/pages/productList/components/productMap/products.tsx
@@ -1,23 +1,16 @@
 import * as ProductsStyle from "./styles";
 import { CategoryFilter } from "../filters/categoryFilter";
 import { MinPriceFilter } from "../filters/minPriceFilter";
-import { useState } from "react";
 import { FilterProductsList } from "./filterProductsList";
 
 export const Products = () => {
-  const [active, setActive] = useState<boolean>(false);
-
   return (
     <ProductsStyle.MainContainer>
       <MinPriceFilter />
-      <label>
-        <p>Mais populares primeiro</p>
-        <input type="checkbox" onChange={() => setActive(!active)} />
-      </label>
       <CategoryFilter />
       <ProductsStyle.ProductContainer>
         <ProductsStyle.ItemsSubGrid>
-          <FilterProductsList active={active} />
+          <FilterProductsList />
         </ProductsStyle.ItemsSubGrid>
       </ProductsStyle.ProductContainer>
     </ProductsStyle.MainContainer>
