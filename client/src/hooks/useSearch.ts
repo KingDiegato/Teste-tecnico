d81adef509f0905a32e0d478b959ab5cb0ea9587
KingDiegato <gonzalezbusnego@gmail.com>
2024-06-30 12:57:24 -0300

    feat: create use search hook

---
+++ client/src/hooks/useSearch.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { Search } from "../types/search";
+
+export const useSearch = ({ query }: { query: string }) => {
+  const [search, setSearch] = useState<Search | null>(null);
+  const [error, setError] = useState<string | unknown>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const searchProducts = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.API_URL}products/search/${query}`
+        );
+        const data = await response.json();
+        setSearch(data);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (query) {
+      setLoading(true);
+      searchProducts();
+    }
+  }, [query]);
+  return {
+    search,
+    error,
+    loading,
+  };
+};
